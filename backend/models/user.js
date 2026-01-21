d27fe9ec79c8212e6cd5a96c32bc94a05689b7dd
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-01-21 17:56:53 +0100

    Add user methods

--- backend/models/user.js
+++ backend/models/user.js
@@ -31,6 +31,56 @@ const userSchema = new Schema(
 );
 
 /* Create methods */
+userSchema.statics.register = async function (name, email, password) {
+  if (!name || !email || !password) {
+    throw new Error("All fields required");
+  }
+
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw new Error("User already exists");
+  }
+
+  const newUser = await this.create({
+    name,
+    email,
+    password,
+  });
+
+  return newUser;
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw new Error("All fields required");
+  }
+
+  const currentUser = await this.findOne({ email }).select("+password");
+  if (!currentUser) {
+    throw new Error("User does not exist yet");
+  }
+
+  const match = await bcrypt.compare(password, currentUser.password);
+  if (!match) {
+    throw new Error("Password is invalid");
+  }
+
+  const user = await this.findOne({ email }).select("-password");
+
+  return user;
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password") || !this.password) {
+    return;
+  }
+
+  try {
+    this.password = await bcrypt.hash(this.password);
+  } catch (error) {
+    next(error);
+  }
+});
 
 /* Create export */
 module.exports = mongoose.model("User", userSchema);
