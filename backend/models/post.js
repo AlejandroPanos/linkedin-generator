73a775b2c93bf287b8ddd7541305bc44ee37e007
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-01-22 19:09:57 +0100

    Fix Post schema

--- backend/models/post.js
+++ backend/models/post.js
@@ -8,6 +8,11 @@ const postSchema = new Schema(
     userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: [true, "Content is required"],
     },
     topic: {
       type: String,
@@ -15,15 +20,22 @@ const postSchema = new Schema(
     },
     tone: {
       type: String,
+      enum: ["professional", "casual", "thought-leadership", "storytelling"],
       required: [true, "Tone is required"],
     },
     length: {
-      enum: ["short", "medium", "long"],
+      type: Number,
+      min: 50,
+      max: 300,
       required: [true, "Length is required"],
     },
     context: {
       type: String,
-      required: false,
+      required: [true, "Context is required"],
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
     },
   },
   { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } },
