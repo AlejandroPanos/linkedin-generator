a3778cbda96a5a38ed60e1e3d34bf065e0920512
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-01-26 09:07:00 +0100

    Finish UI

---
+++ frontend/src/components/private/SettingsActions/SettingsActions.tsx
@@ -0,0 +1,24 @@
+const SettingsActions = () => {
+  return (
+    <>
+      <div className="content-wrapper">
+        <h2>Change or cancel plan</h2>
+        <p>
+          You are currently in the Free Plan. If you want to upgrade to Business Plan{" "}
+          <a href="#">click here</a>.
+        </p>
+        <p>
+          If you want to cancel your plan, <a href="#">click here</a>.
+        </p>
+
+        <h2 className="mt-6">Delete account</h2>
+        <p>
+          Please <a href="#">click here</a> to contact our support team if you want to delete your
+          account.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default SettingsActions;
--- frontend/src/pages/private/Support.tsx
+++ frontend/src/pages/private/Support.tsx
@@ -1,7 +1,16 @@
+import PageHeader from "../../components/private/PageHeader/PageHeader";
+import SupportDetails from "../../components/private/SupportDetails/SupportDetails";
+
 const Support = () => {
   return (
     <>
-      <h1>Support Page</h1>
+      <div className="page-wrapper">
+        <PageHeader
+          title="Support"
+          text="Contact our support team for anything related to your account."
+        />
+        <SupportDetails />
+      </div>
     </>
   );
 };
