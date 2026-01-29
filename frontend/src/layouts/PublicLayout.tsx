b6c636ff612ba2f52c71831b807c0ac1741f8459
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-01-29 17:54:22 +0100

    change/responsive-design

--- frontend/src/layouts/PublicLayout.tsx
+++ frontend/src/layouts/PublicLayout.tsx
@@ -5,9 +5,9 @@ import Navbar from "../components/public/Navbar/Navbar";
 const PublicLayout = (): React.JSX.Element => {
   return (
     <>
-      <div className="mx-auto px-16 py-8 max-w-300 flex flex-col items-center">
+      <div className="mx-auto px-12 md:px-16 py-8 max-w-300 flex flex-col items-center">
         <Navbar />
-        <main className="w-full mt-56">
+        <main className="w-full mt-32 md:mt-56">
           <Outlet />
         </main>
       </div>
