3516fe2fcbde45091e0288317c18a1c43dca805c
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-01-30 16:10:41 +0100

    fix/import-loader-css

--- frontend/src/components/state/Loader/Loader.tsx
+++ frontend/src/components/state/Loader/Loader.tsx
@@ -1,6 +1,8 @@
+import "./Loader.css";
+
 const Loader = (): React.JSX.Element => {
   return (
-    <div className="loader-outter">
+    <div className="loader-outter" role="status" aria-live="polite">
       <div className="loader-inner"></div>
     </div>
   );
