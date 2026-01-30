9eb8548fbb296c06b9c273a6fd907c0b6a61793b
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-01-30 18:16:11 +0100

    change/demo-video

--- frontend/src/components/public/Hero/Hero.tsx
+++ frontend/src/components/public/Hero/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
             <div className="video-holder">
               <iframe
                 title="Product demo video"
-                src="https://www.youtube.com/embed/tgbNymZ7vqY"
+                src="https://youtu.be/5U2l-zX_b6A"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
               />
